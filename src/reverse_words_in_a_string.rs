60f8160d40a98f034e0757c20b7787542d8022c2
DPR <dpr-0xff@pm.me>
2023-12-23 14:52:07 +0800

    add reverse_words_in_a_string

---
+++ src/reverse_words_in_a_string.rs
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=151 lang=rust
+ *
+ * [151] Reverse Words in a String
+ */
+use crate::Solution;
+// @lc code=start
+#[allow(dead_code)]
+impl Solution {
+    pub fn reverse_words(s: String) -> String {
+        let mut res = Vec::new();
+        for w in s.split_whitespace().rev() {
+            res.push(w);
+        }
+        res.join(" ")
+    }
+}
+// @lc code=end
+
+#[test]
+fn test_reverse_words() {
+    let case1 = "the sky is blue";
+    assert_eq!(
+        Solution::reverse_words(case1.to_string()),
+        "blue is sky the"
+    );
+}
