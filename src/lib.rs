188649bd29ea143293d984abd29f39a2f6716a82
DPR <dpr-0xff@pm.me>
2024-01-05 16:54:35 +0800

    add longest_consecutive_sequence.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod convert_an_array_into_a_2d_array_with_conditions;
 mod encode_and_decode_strings;
 mod group_anagrams;
+mod longest_consecutive_sequence;
 mod minimum_difficulty_of_a_job_schedule;
 mod product_of_array_except_self;
 mod redistribute_characters_to_make_all_strings_equal;
---
+++ src/longest_consecutive_sequence.rs
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=128 lang=rust
+ *
+ * [128] Longest Consecutive Sequence
+ *
+ * https://leetcode.com/problems/longest-consecutive-sequence/description/
+ *
+ * algorithms
+ * Medium (47.40%)
+ * Likes:    19031
+ * Dislikes: 895
+ * Total Accepted:    1.5M
+ * Total Submissions: 3.2M
+ * Testcase Example:  '[100,4,200,1,3,2]'
+ *
+ * Given an unsorted array of integers nums, return the length of the longest
+ * consecutive elements sequence.
+ *
+ * You must write an algorithm that runs in O(n) time.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [100,4,200,1,3,2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
+ * Therefore its length is 4.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * Output: 9
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ *
+ *
+ */
+
+use crate::Solution;
+// @lc code=start
+use std::iter::FromIterator;
+use std::{cmp, collections::HashSet};
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        let nums: HashSet<i32> = HashSet::from_iter(nums.into_iter());
+
+        let mut longest_consecutive_len = 0;
+        for num in &nums {
+            if !nums.contains(&(num - 1)) {
+                let mut consecutive_len = 1;
+                let mut i = 1;
+                loop {
+                    if nums.contains(&(num + i)) {
+                        consecutive_len += 1;
+                        i += 1;
+                    } else {
+                        break;
+                    }
+                }
+                longest_consecutive_len = cmp::max(longest_consecutive_len, consecutive_len);
+            }
+        }
+        longest_consecutive_len as i32
+    }
+}
+// @lc code=end
+
+#[test]
+fn test_longest_consecutive() {
+    let case1 = vec![100, 4, 200, 1, 3, 2];
+    assert_eq!(Solution::longest_consecutive(case1), 4);
+
+    let case2 = vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+    assert_eq!(Solution::longest_consecutive(case2), 9);
+}
